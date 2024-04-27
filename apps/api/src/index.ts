802df95ee883c8cfaf91c4e427b3a8719c96e271
nulfrost <khadane.miller@gmail.com>
2024-04-27 16:42:47 -0400

    more minor setup

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,6 +2,6 @@ import { Elysia } from "elysia";
 
 new Elysia()
   .get("/", () => "precis api server")
-  .listen(Bun.env.PORT, () =>
-    console.log(`Running API server @ http://localhost:${Bun.env.PORT}`),
+  .listen(process.env.PORT, () =>
+    console.log(`Running API server @ http://localhost:${process.env.PORT}`),
   );
