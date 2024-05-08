9e2872da2a3053125747441b23fc39a1a7bada57
nulfrost <khadane.miller@gmail.com>
2024-05-07 23:38:01 -0400

    feat: working on the api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,45 @@
 import { Elysia } from "elysia";
+import { db, eq, schema } from "@precis/database";
+import logixlysia from "logixlysia";
 
-new Elysia()
-  .get("/", () => "precis api server")
-  .listen(process.env.PORT, () =>
-    console.log(`Running API server @ http://localhost:${process.env.PORT}`),
-  );
+new Elysia({ name: "Precis API" })
+  .use(
+    logixlysia({
+      config: {
+        ip: true,
+        customLogFormat:
+          "{now} {level} {duration} {method} {pathname} {status} {message} {ip}",
+      },
+    }),
+  )
+  .group("/api/v1", (app) =>
+    app
+      .get(
+        "/guestbooks/:username/messages",
+        async ({ params: { username } }) => {
+          const users = await db.query.users.findFirst({
+            columns: {
+              id: true,
+              username: true,
+            },
+            with: {
+              guestbook: {
+                with: {
+                  messages: true,
+                },
+              },
+            },
+            where: eq(schema.users.username, username),
+          });
+
+          return users;
+        },
+      )
+      .post(
+        "/guestbooks/:username/messages",
+        async ({ params: { username } }) => {
+          return "not impl";
+        },
+      ),
+  )
+  .listen(process.env.PORT || 4000);
