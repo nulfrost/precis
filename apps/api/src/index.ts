9acde085424def078210887478b13f0afe22151b
nulfrost <khadane.miller@gmail.com>
2024-05-09 23:55:41 -0400

    feat(api): add logging and some other reformatting

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,18 +1,123 @@
-import { Elysia, t } from "elysia";
+import { Elysia, NotFoundError, t } from "elysia";
 import { db, eq, schema } from "@precis/database";
-import logixlysia from "logixlysia";
+import {
+  AuthenticationError,
+  BadRequestError,
+  sendErrorResponse,
+  sendSuccessResponse,
+} from "../utils";
+import { HttpStatusCode } from "elysia-http-status-code";
+import { HttpStatusEnum } from "elysia-http-status-code/status";
+import { helmet } from "elysia-helmet";
+import { createPinoLogger, formatters } from "@bogeychan/elysia-logger";
+import { ip } from "elysia-ip";
+
+const log = createPinoLogger({
+  level: process.env.LOG_LEVEL || "info",
+  formatters: {
+    ...formatters,
+    bindings(bindings) {
+      return {
+        pid: bindings.pid,
+        host: bindings.hostname,
+        time: new Date().toISOString(),
+      };
+    },
+    level(label, _) {
+      return { level: label.toUpperCase() };
+    },
+    log(object) {
+      return formatters.log(object);
+    },
+  },
+});
 
 new Elysia({ name: "Precis API" })
-  .use(
-    logixlysia({
-      config: {
-        ip: true,
-        customLogFormat:
-          "{now} {level} {duration} {method} {pathname} {status} {message} {ip}",
-      },
-    }),
-  )
-
+  .use(helmet())
+  .use(ip())
+  .use(HttpStatusCode())
+  .use(log.into({ autoLogging: false }))
+  .error({
+    AUTHENTICATION_ERROR: AuthenticationError,
+    BAD_REQUEST_ERROR: BadRequestError,
+  })
+  .onError(({ code, error, httpStatus, set, ip, params: { guestbookId } }) => {
+    switch (code) {
+      case "AUTHENTICATION_ERROR":
+        set.status = 401;
+        log.error(
+          {
+            status: httpStatus.HTTP_401_UNAUTHORIZED,
+            ip,
+            err: {
+              stack: error.stack,
+            },
+            guestbookId,
+          },
+          error.message.toString(),
+        );
+        return sendErrorResponse({
+          status: httpStatus.HTTP_401_UNAUTHORIZED,
+          title: "Error: unauthorized",
+          detail: error.message.toString(),
+        });
+      case "BAD_REQUEST_ERROR":
+        set.status = 400;
+        log.error(
+          {
+            status: httpStatus.HTTP_400_BAD_REQUEST,
+            ip,
+            err: {
+              stack: error.stack,
+            },
+            guestbookId,
+          },
+          error.message.toString(),
+        );
+        return sendErrorResponse({
+          status: httpStatus.HTTP_400_BAD_REQUEST,
+          title: "Error: could not find guestbook",
+          detail: error.message.toString(),
+        });
+      case "NOT_FOUND":
+        log.error(
+          {
+            status: httpStatus.HTTP_404_NOT_FOUND,
+            ip,
+            err: {
+              stack: error.stack,
+            },
+            guestbookId,
+          },
+          error.message.toString(),
+        );
+        return sendErrorResponse({
+          status: httpStatus.HTTP_404_NOT_FOUND,
+          title: "Error: resource not found",
+          detail:
+            "Resource requested could not be found on the server. Please make sure your request is valid.",
+        });
+      case "INTERNAL_SERVER_ERROR":
+        set.status = 500;
+        log.error(
+          {
+            status: httpStatus.HTTP_500_INTERNAL_SERVER_ERROR,
+            ip,
+            err: {
+              stack: error.stack,
+            },
+            guestbookId,
+          },
+          error.message.toString(),
+        );
+        return sendErrorResponse({
+          status: httpStatus.HTTP_500_INTERNAL_SERVER_ERROR,
+          title: "Error: unhandled error",
+          detail:
+            "An unhandled error occurred on the server. Please try again later.",
+        });
+    }
+  })
   .group("/api/v1", (app) =>
     app
       .guard({
@@ -20,21 +125,14 @@ new Elysia({ name: "Precis API" })
           guestbookId: t.String(),
         }),
         headers: t.Object({
-          "X-Precis-Key": t.String(),
+          "x-precis-key": t.String(),
         }),
-        // @ts-ignore
-        async beforeHandle({ set, params: { guestbookId }, headers }) {
+        async beforeHandle({ params: { guestbookId }, headers }) {
           // have to check if api key is valid and exists
-          if (!headers["X-Precis-Key"]) {
-            set.status = "Unauthorized";
-            return {
-              data: {
-                status: "401",
-                title: "Error: unauthorized",
-                detail:
-                  "Invalid API key provided. Please provide a valid API key to access this resource.",
-              },
-            };
+          if (!headers["x-precis-key"]) {
+            throw new AuthenticationError(
+              "Invalid API key provided. Please provide a valid API key to access this resource.",
+            );
           }
           // have to check if guestbook exists
           const guestbook = await db.query.guestbooks.findFirst({
@@ -42,14 +140,9 @@ new Elysia({ name: "Precis API" })
           });
 
           if (typeof guestbook === "undefined") {
-            set.status = "Bad Request";
-            return {
-              data: {
-                status: "400",
-                title: "Error: could not find guestbook",
-                detail: `The guestbook with the id of: "${guestbook}" could not be found on the server. Please double check your API url.`,
-              },
-            };
+            throw new BadRequestError(
+              "The guestbook requested does not exist on the server. Please make sure your guestbook id is valid.",
+            );
           }
         },
       })
@@ -87,11 +180,11 @@ new Elysia({ name: "Precis API" })
           set.status = 201;
 
           return {
-            data: {
-              status: "201",
+            ...sendSuccessResponse({
+              status: HttpStatusEnum.HTTP_201_CREATED,
               title: "Success: message created",
               detail: "The message has been successfully created.",
-            },
+            }),
           };
         },
         {
@@ -102,4 +195,7 @@ new Elysia({ name: "Precis API" })
         },
       ),
   )
+  .all("*", () => {
+    throw new NotFoundError();
+  })
   .listen(process.env.PORT || 4000);
