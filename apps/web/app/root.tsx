3ee9615f962482773b6a0ce2c7d56170c9ab6306
nulfrost <khadane.miller@gmail.com>
2024-05-11 01:18:57 -0400

    feat: add footer with github link

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -12,6 +12,7 @@ import "virtual:uno.css";
 import { Navbar } from "./components/Navbar";
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { authenticator } from "./services/auth.server";
+import { Footer } from "./components/Footer";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const user = await authenticator.isAuthenticated(request);
@@ -21,16 +22,17 @@ export async function loader({ request }: LoaderFunctionArgs) {
 export function Layout({ children }: { children: React.ReactNode }) {
   const { user } = useLoaderData<typeof loader>();
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body className="bg-gray-50">
+      <body className="bg-gray-50 flex flex-col h-full">
         <Navbar user={user} />
-        <main className="max-w-7xl mx-auto">{children}</main>
+        <main className="max-w-7xl mx-auto flex-1">{children}</main>
+        <Footer />
         <ScrollRestoration />
         <Scripts />
       </body>
