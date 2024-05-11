64c2e5a686c35a14eaa4956dde94227141fcbf7a
nulfrost <khadane.miller@gmail.com>
2024-05-11 12:10:51 -0400

    feat: add ratelimiting to regenerating key and a toast notification

--- apps/web/app/routes/regenerate.tsx
+++ apps/web/app/routes/regenerate.tsx
@@ -2,9 +2,19 @@ import { ActionFunctionArgs, redirect, json } from "@remix-run/node";
 import { authenticator } from "../services/auth.server";
 import { db, eq, schema } from "@precis/database";
 import { generateApiKey } from "../utils/generateApiKey.server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import { jsonWithError } from "remix-toast";
+
+const regenerateRateLimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(5, "1 h"),
+  analytics: true,
+});
 
 export async function action({ request }: ActionFunctionArgs) {
   const user = await authenticator.isAuthenticated(request);
+
   if (!user) {
     return json(
       {
@@ -15,6 +25,29 @@ export async function action({ request }: ActionFunctionArgs) {
         },
       },
       { status: 401 },
+    );
+  }
+
+  const { limit, remaining, reset, success } = await regenerateRateLimit.limit(
+    user!.id!,
+  );
+
+  if (!success) {
+    return jsonWithError(
+      null,
+      {
+        message: "Error: too many requests",
+        description:
+          "You have exceeded the rate limit for this request. Please try again later.",
+      },
+      {
+        status: 429,
+        headers: {
+          "x-rate-limit-limit": limit.toString(),
+          "x-rate-limit-remaining": remaining.toString(),
+          "x-rate-limit-reset": reset.toString(),
+        },
+      },
     );
   }
 
