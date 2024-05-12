b194e5672f1feba4be2a112ae932de08fd972d82
Dane Miller <khadane.miller@gmail.com>
2024-05-12 17:11:50 -0400

    feat: modify start commands according to the railway docs (#23)

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -3,11 +3,14 @@ import { createClient } from "@libsql/client";
 
 import * as schema from "../src/drizzle/schema";
 
+console.log(process.env.DATABASE_URL!, process.env.DATABASE_AUTH_TOKEN!);
+
 // For production we'll use Turso
 const sqlite = createClient({
   url: process.env.DATABASE_URL!,
   authToken: process.env.DATABASE_AUTH_TOKEN!,
 });
+
 const db = drizzle(sqlite, { schema });
 
 export type User = typeof schema.users.$inferInsert;
